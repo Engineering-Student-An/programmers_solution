70027c3685867a4a3fc7f76be1cad86a882bad27
Engineering-Student-An <139744625+Engineering-Student-An@users.noreply.github.com>
2023-09-03 17:02:01 +0900

    [level 0] Title: 두 수의 차, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120803. 두 수의 차/두 수의 차.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = num1 - num2;
+    return answer;
+}
+int main(void){
+    int num1, num2;
+    cin >> num1 >> num2;
+    int ans = solution(num1, num2);
+    cout << ans;
+}
