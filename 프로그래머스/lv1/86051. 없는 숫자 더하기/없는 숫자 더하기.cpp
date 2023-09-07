21edbb65622336bfb02a0f42d9b3987ea75bce33
Engineering-Student-An <139744625+Engineering-Student-An@users.noreply.github.com>
2023-09-07 13:02:58 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 4.23 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    int answer = 0;
+    int arr[10]={0};
+    for(int i=0;i<size(numbers);i++){
+        arr[numbers[i]]++;
+    }
+    for(int i=0;i<10;i++){
+        if(arr[i]==0) answer+=i;
+    }
+    return answer;
+}
