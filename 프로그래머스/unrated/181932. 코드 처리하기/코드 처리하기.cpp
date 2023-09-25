67bd43a4fbe1f41802c96127f84f131ce6b59821
Engineering-Student-An <139744625+Engineering-Student-An@users.noreply.github.com>
2023-09-25 12:38:56 +0900

    [unrated] Title: 코드 처리하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string code) {
+    string answer = "";
+    int mode=0;
+    for(int i=0;i<size(code);i++){
+        if(code[i]=='1') mode = (mode+1) % 2;
+        else{
+            if(mode==0 && i%2==0) answer+=code[i];
+            else if(mode==1 && i%2==1) answer+=code[i];
+        }
+    }
+    if(answer=="") answer="EMPTY";
+    return answer;
+}
