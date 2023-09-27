88f9c152b944ef19981117d35ef52406f68b55ae
Engineering-Student-An <139744625+Engineering-Student-An@users.noreply.github.com>
2023-09-27 22:25:42 +0900

    [unrated] Title: 이어 붙인 수, Time: 0.02 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    string odd="", even="";
+    for(int i=0;i<size(num_list);i++){
+        if(num_list[i]%2==0) even+=to_string(num_list[i]);
+        else odd+=to_string(num_list[i]);
+    }
+    answer = stoi(even) + stoi(odd);
+    return answer;
+}
