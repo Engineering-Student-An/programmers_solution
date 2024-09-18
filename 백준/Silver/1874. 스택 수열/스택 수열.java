b044883fd835e941afd57b721085af43cef0e51b
Engineering-Student-An <139744625+Engineering-Student-An@users.noreply.github.com>
2024-09-18 17:00:21 +0900

    [Silver II] Title: 스택 수열, Time: 876 ms, Memory: 112384 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -3,39 +3,37 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
+        Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
+
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = scanner.nextInt();
         }
 
-        Stack<Integer> stack = new Stack<>();
+        int next = 0;
         StringBuilder sb = new StringBuilder();
-        int cnt = 1;
-        boolean isFinish = false;
-        for (int i = 0; i < n; i++) {
-            if(stack.isEmpty()) {
-                stack.push(cnt++);
-                sb.append("+" + "\n");
-            }
-            while(stack.peek()!=arr[i]){
-                stack.push(cnt++);
-                sb.append("+" + "\n");
-                if(cnt > n+1) {
-                    isFinish = true;
+        Stack<Integer> stack = new Stack<>();
+        int plus = 0;
+        int minus = 0;
+        for (int i = 1; i <= n; i++) {
+            stack.push(i);
+            sb.append("+").append("\n");
+            plus ++;
+
+            while (!stack.isEmpty() && next < n) {
+                if(arr[next] == stack.peek()) {
+                    minus++;
+                    sb.append("-").append("\n");
+                    stack.pop();
+                    next++;
+                } else {
                     break;
                 }
             }
-            if(isFinish){
-                sb.delete(0, sb.length());
-                sb.append("NO");
-                break;
-            }
-            stack.pop();
-            sb.append("-" + "\n");
         }
-        System.out.print(sb);
+
+        System.out.print((plus == minus) ? sb : "NO");
     }
 }
