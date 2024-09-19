74f1dae0f55a405b1fb5fd3706f099ad70bacd59
Engineering-Student-An <139744625+Engineering-Student-An@users.noreply.github.com>
2024-09-19 21:01:38 +0900

    [Silver IV] Title: 카드2, Time: 236 ms, Memory: 48904 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -3,30 +3,24 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
         int n = scanner.nextInt();
 
         Deque<Integer> deque = new LinkedList<>();
 
         for (int i = 1; i <= n; i++) {
-            deque.add(i);
+            deque.addLast(i);
         }
 
-        while(deque.size() > 1) {
-            deque.poll();
-
-            if(deque.size() == 1) {
-                break;
-            }
-            int a = deque.poll();
-            deque.addLast(a);
-
+        while (deque.size() > 1) {
+            deque.removeFirst();
+            Integer i = deque.removeFirst();
+            deque.addLast(i);
         }
 
         System.out.println(deque.poll());
     }
-
 }
