c9d7c3e2c9bc6c2af57baab2a8c1d32f2f5e6602
Engineering-Student-An <139744625+Engineering-Student-An@users.noreply.github.com>
2024-09-24 16:05:18 +0900

    [Gold II] Title: 버블 소트, Time: 612 ms, Memory: 61756 KB -BaekjoonHub

--- 백준/Gold/1377. 버블 소트/버블 소트.java
+++ 백준/Gold/1377. 버블 소트/버블 소트.java
@@ -1,10 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,36 +12,39 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
 
         int[] arr = new int[n];
-        Ddata[] sorted = new Ddata[n];
-
+        PriorityQueue<Data> q = new PriorityQueue<>(new QComparator());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
-            sorted[i] = new Ddata(arr[i], i);
+            q.add(new Data(arr[i], i));
         }
-
-        Arrays.sort(sorted);
-
         int max = -1;
         for (int i = 0; i < n; i++) {
-            if(sorted[i].index - i > max) {
-                max = sorted[i].index - i;
-            }
+            Data data = q.poll();
+            if(max < data.index - i) max = data.index - i;
         }
+
         System.out.println(max + 1);
+
     }
 
-    static class Ddata implements Comparable<Ddata>{
+    static class Data {
         int value;
         int index;
 
-        public Ddata(int value, int index) {
+        public Data(int value, int index) {
             this.value = value;
             this.index = index;
         }
+    }
+
+    static class QComparator implements Comparator<Data> {
 
         @Override
-        public int compareTo(Ddata o) {
-            return this.value - o.value;
+        public int compare(Data o1, Data o2) {
+            if(o1.value == o2.value) {
+                return o1.index - o2.index;
+            }
+            return o1.value - o2.value;
         }
     }
 }
