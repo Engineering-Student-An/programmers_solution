6c69f1aac1e9081d747293a4da7b5051ee7f477e
Engineering-Student-An <139744625+Engineering-Student-An@users.noreply.github.com>
2024-10-06 22:11:36 +0900

    [Silver IV] Title: ATM, Time: 236 ms, Memory: 21232 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,23 +1,36 @@
-import java.math.BigInteger;
-import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
         int n = scanner.nextInt();
+
         int[] arr = new int[n];
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             arr[i] = scanner.nextInt();
         }
 
-        Arrays.sort(arr);
-
-        BigInteger sum = BigInteger.ZERO;
-        for(int i=0;i<n;i++){
-            sum = sum.add(BigInteger.valueOf(arr[i]*(n-i)));
+        for (int i = 1; i < n; i++) {
+            int now = arr[i];
+            int ind = i;
+            for (int j = i-1; j >= 0; j--) {
+                if(arr[i] < arr[j]) {
+                    ind = j;
+                } else break;
+            }
+            for (int j = i-1; j >= ind; j--) {
+                arr[j+1] = arr[j];
+            }
+            arr[ind] = now;
         }
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += (n-i) * arr[i];
+        }
+
         System.out.println(sum);
     }
 }
