7372fa4c29d29ef2ae878102b44bd2c8147d43e7
Engineering-Student-An <chm20060@gmail.com>
2024-10-28 15:54:55 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1092 ms, Memory: 132592 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,22 +1,62 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
-public class Main{
-    public static void main(String[] args) {
+public class Main {
+    public static int[] temp;
+    public static void main(String[] args) throws IOException {
 
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        ArrayList<Integer> arr = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        temp = new int[n];
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-            arr.add(scanner.nextInt());
+            st = new StringTokenizer(br.readLine());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        Collections.sort(arr);
-        StringBuilder sb = new StringBuilder();
-        for (Integer integer : arr) {
-            sb.append(integer);
-            sb.append('\n');
+
+        mergeSort(arr, 0, n-1);
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i : arr) {
+            bw.write(i + "\n");
         }
-        System.out.print(sb);
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static void mergeSort(int[] arr, int start, int end) {
+
+        if(start >= end) return;
+
+        int middle = (start + end) / 2;
+
+        mergeSort(arr, start, middle);
+        mergeSort(arr, middle+1, end);
+
+        for (int i = start; i <= end; i++) {
+            temp[i] = arr[i];
+        }
+
+        int k = start;
+        int pl = start;
+        int pr = middle+1;
+
+        while(pl <= middle && pr <= end){
+            if(temp[pl] < temp[pr]) {
+                arr[k++] = temp[pl++];
+            } else {
+                arr[k++] = temp[pr++];
+            }
+        }
+
+        while(pl <= middle) {
+            arr[k++] = temp[pl++];
+        }
+        while(pr <= end) {
+            arr[k++] = temp[pr++];
+        }
     }
 }
