a77fdea991d6d4d126e58f85ed605e3cdac43f0a
Engineering-Student-An <chm20060@gmail.com>
2024-10-29 16:15:54 +0900

    [Silver I] Title: 미로 탐색, Time: 116 ms, Memory: 14688 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,55 +1,54 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    static int n, m;
-    static int[][] arr;
-    static boolean[][] visit;
-    static int[] dir_x = {1, 0, -1, 0};
-    static int[] dir_y = {0, 1, 0, -1};
+    public static void main(String[] args) throws IOException {
 
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = scanner.nextInt();
-        m = scanner.nextInt();
-        arr = new int [n][m];
-        visit = new boolean[n][m];
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < n; i++) {
-            String line = scanner.next();
-            for (int j = 0; j < m; j++) {
-                arr[i][j] = (int) (line.charAt(j) - '0');
+        boolean[][] arr = new boolean[n+2][m+2];
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            String line = st.nextToken();
+            for (int j = 1; j <= m; j++) {
+                arr[i][j] = (line.charAt(j - 1) == '1');
             }
         }
 
-        visit[0][0] = true;
-        bfs(0,0);
-        System.out.println(arr[n-1][m-1]);
-    }
-
-    private static void bfs(int y, int x) {
+        int[][] check = new int[n+1][m+1];
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[] {y, x});
 
-        while (!queue.isEmpty()) {
-            int[] temp = queue.poll();
-            int now_y = temp[0];
-            int now_x = temp[1];
+        int[] dr = {0, 1, 0, -1};
+        int[] dc = {1, 0, -1, 0};
+
+        queue.add(new int[]{1, 1, 1});
+        check[1][1] = 1;
+        while(!queue.isEmpty()) {
+            int[] poll = queue.poll();
+
+            int r = poll[0];
+            int c = poll[1];
 
             for (int i = 0; i < 4; i++) {
-                int next_y = now_y + dir_y[i];
-                int next_x = now_x + dir_x[i];
-                if(next_y >= 0 && next_y < n && next_x >= 0 && next_x < m && arr[next_y][next_x] != 0
-                        && !visit[next_y][next_x]){
-                    queue.add(new int[]{next_y, next_x});
-                    arr[next_y][next_x] = arr[now_y][now_x] + 1;
-                    visit[next_y][next_x] = true;
+                int nextRow = r+dr[i];
+                int nextCol = c+dc[i];
+                if(arr[nextRow][nextCol] && check[nextRow][nextCol] == 0) {
+                    queue.add(new int[]{nextRow, nextCol, poll[2]+1});
+                    check[nextRow][nextCol] = poll[2]+1;
                 }
             }
-
         }
+
+        System.out.println(check[n][m]);
     }
 }
-
