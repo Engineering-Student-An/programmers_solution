50578910ac781e6a718d8f1a71a656be4420528b
Engineering-Student-An <chm20060@gmail.com>
2024-10-31 15:12:02 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 376 ms, Memory: 29476 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -1,32 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner scanner = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = scanner.nextInt();
+        int n = Integer.parseInt(st.nextToken());
 
         PriorityQueue<Integer> queue = new PriorityQueue<>();
 
         for (int i = 0; i < n; i++) {
-            queue.add(scanner.nextInt());
+            st = new StringTokenizer(br.readLine());
+            queue.add(Integer.parseInt(st.nextToken()));
         }
-
-        int ans = 0;
-
-        if(queue.size() > 1) {
-
-            while (queue.size() > 1) {
-                int a = queue.poll();
-                int b = queue.poll();
-                int temp = a + b;
-                ans += temp;
-                queue.add(temp);
+        if(n == 1) {
+            System.out.println(0);
+        } else {
+            long ans = 0;
+            while(queue.size() > 1) {
+                int next = queue.poll() + queue.poll();
+                ans += next;
+                queue.add(next);
             }
+            System.out.println(ans);
         }
 
-        System.out.println(ans);
     }
 }
