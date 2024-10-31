d8d03df0b5d75fbd4970b58ae14c54ab4bf13951
Engineering-Student-An <chm20060@gmail.com>
2024-10-31 16:32:29 +0900

    [Silver I] Title: 회의실 배정, Time: 1260 ms, Memory: 226912 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -7,27 +7,28 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        long[][] arr = new long[n][2];
+
+        int[][] arr = new int[n][2];
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < 2; j++) {
-                arr[i][j] = scanner.nextLong();
+                arr[i][j] = scanner.nextInt();
             }
         }
 
-        Arrays.sort(arr, Comparator.comparing((long[] o) -> o[1])
-                .thenComparing((long[] o) -> o[0]));
+        Arrays.sort(arr, Comparator.comparing((int[] o) -> o[1])
+                .thenComparing((int[] o) -> o[0]));
 
-        long end = 0;
         int count = 0;
+        int end = 0;
 
         for (int i = 0; i < n; i++) {
-            if(arr[i][0] >= end){
-                count ++;
+            if(arr[i][0] >= end) {
                 end = arr[i][1];
+                count ++;
             }
         }
 
         System.out.println(count);
     }
 }
-
