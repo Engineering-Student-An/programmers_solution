0a3535f760e5da86cca1f00c7bc3249323d2d8bc
Engineering-Student-An <chm20060@gmail.com>
2024-11-25 23:35:50 +0900

    [Gold IV] Title: 거짓말, Time: 196 ms, Memory: 18048 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -2,69 +2,83 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
+    static int[] arr;
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        ArrayList<Integer> truthKnow = new ArrayList<>();
+        int p = scanner.nextInt();
 
-        int truth = scanner.nextInt();
-        for(int i=0;i<truth;i++){
-            truthKnow.add(scanner.nextInt());
+        int know = scanner.nextInt();
+        ArrayList<Integer> knowList = new ArrayList<>();
+        for (int i = 0; i < know; i++) {
+            knowList.add(scanner.nextInt());
         }
 
-        int[] parent = new int[n+1];
-        for(int i=1;i<=n;i++) parent[i]=i;
+        arr = new int[n+1];
+        for (int i = 0; i <= n; i++) {
+            arr[i] = i;
+        }
+        for (Integer integer : knowList) {
+            arr[integer] = 0;
+        }
 
-        ArrayList[] party = new ArrayList[m];
-        for(int i=0;i<m;i++){
-            party[i] = new ArrayList<>();
-            int p = scanner.nextInt();
-            for (int j = 0; j < p; j++) {
-                party[i].add(scanner.nextInt());
+        ArrayList<Integer>[] partyList = new ArrayList[p];
+        for (int i = 0; i < p; i++) {
+            partyList[i] = new ArrayList<>();
+
+            int people = scanner.nextInt();
+            for (int j = 0; j < people; j++) {
+                partyList[i].add(scanner.nextInt());
             }
         }
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < party[i].size(); j++) {
-                for (int k = 0; k < party[i].size(); k++) {
-
-                        union((Integer) party[i].get(j),(Integer) party[i].get(k), parent);
-
-
+        for (int i = 0; i < p; i++) {
+            for (Integer integer : partyList[i]) {
+                for (Integer integer1 : partyList[i]) {
+                    union(integer, integer1);
                 }
             }
         }
-        int sum = 0;
-        for (int i = 0; i < m; i++) {
-            if(!isContain(party[i], truthKnow,parent)) {
-                sum++;
+
+        int count = 0;
+        for (int i = 0; i < p; i++) {
+            boolean isContain = false;
+            int current = partyList[i].get(0);
+            for (int j = 0; j < knowList.size(); j++) {
+                if(find(current).equals(find(knowList.get(j)))) {
+                    isContain = true;
+                    break;
+                }
+            }
+
+            if(!isContain) count ++;
+        }
+
+        System.out.println(count);
+    }
+
+    private static void union(Integer a, Integer b) {
+
+        a = find(a);
+        b = find(b);
+
+        if(!a.equals(b)) {
+            if(a < b) {
+                arr[b] = a;
+            } else {
+                arr[a] = b;
             }
         }
-        System.out.println(sum);
     }
 
-    private static boolean isContain(ArrayList<Integer> arrayList, ArrayList<Integer> truthKnow, int[] parent) {
-        for(int i=0;i<arrayList.size();i++){
-            for (int j = 0; j < truthKnow.size(); j++) {
-                if(find( arrayList.get(i), parent) == find(truthKnow.get(j), parent)) return true;
-            }
+    private static Integer find(Integer a) {
+
+        if(arr[a] == a) {
+            return a;
         }
-
-        return false;
+        return arr[a] = find(arr[a]);
     }
-
-
-    private static int find(int i, int[] parent) {
-        if(parent[i]==i) return i;
-        return find(parent[i], parent);
-    }
-    private static void union(int j, int k, int[] parent) {
-        if (find(j, parent) != find(k, parent)) {
-            parent[find(j,parent)] = find(k, parent);
-        }
-    }
-
-
 }
