0ca98d781a5ada144e9714c9aeabf9372b188062
Engineering-Student-An <chm20060@gmail.com>
2025-02-22 15:21:22 +0900

    [Silver II] Title: 이항 계수 2, Time: 188 ms, Memory: 21424 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+
+        int[][] arr = new int[n+1][k+1];
+
+        for (int i = 0; i < n + 1; i++) {
+            arr[i][0] = 1;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < k + 1; j++) {
+
+                arr[i][j] = ((arr[i-1][j] % 10007) + (arr[i-1][j-1] % 10007)) % 10007;
+            }
+        }
+
+        System.out.println(arr[n][k]);
+    }
+}
