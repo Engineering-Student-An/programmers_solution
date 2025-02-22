6c54b027a5e35cec0c5cde6680b4c832a2f1f564
Engineering-Student-An <chm20060@gmail.com>
2025-02-22 15:34:32 +0900

    [Silver V] Title: 다리 놓기, Time: 240 ms, Memory: 19992 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,42 +1,28 @@
-import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
+        long[][] arr = new long[31][31];
+        for (int i = 0; i < 31; i++) {
+            arr[i][0] = 1;
+        }
+
+        for (int i = 1; i < 31; i++) {
+            for (int j = 1; j < 31; j++) {
+                arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
+            }
+        }
+
         int t = scanner.nextInt();
-        BigInteger[][] arr = new BigInteger [30][30];
-        for (int i = 0; i < 30; i++) {
-            for (int j = 0; j < 30; j++) {
-                arr[i][j] = BigInteger.ZERO;
-            }
-        }
-        BigInteger[] fact = new BigInteger[30];
-        fact[0] = BigInteger.ONE; fact[1] = BigInteger.ONE;
-        for(int i=2;i<30;i++){
-            fact[i] = fact[i-1].multiply(BigInteger.valueOf(i));
-        }
-        for(int i=0;i<t;i++){
 
+        for (int tc = 0; tc < t; tc++) {
+            int k = scanner.nextInt();
             int n = scanner.nextInt();
-            int m = scanner.nextInt();
 
-            // m C n
-
-            if(!arr[m][n].equals(BigInteger.ZERO)) {
-                System.out.println(arr[m][n]);
-            }
-            else{
-                for (int j = 0; j <= m / 2; j++) {
-                    arr[m][j] = fact[m].divide(fact[m-j].multiply(fact[j]));
-                }
-                for(int j=m/2+1;j<=m;j++){
-                    arr[m][j] = arr[m][m-j];
-                }
-                System.out.println(arr[m][n]);
-            }
+            System.out.println(arr[n][k]);
         }
-
     }
 }
