95dd585bce9d822ac1726553d733e441294b8313
Engineering-Student-An <chm20060@gmail.com>
2025-02-24 16:50:26 +0900

    [Silver III] Title: 1로 만들기, Time: 216 ms, Memory: 25644 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,23 +1,25 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
         int n = scanner.nextInt();
-        int[] arr = new int[n+1];
-        for (int i = 2; i <= n; i++) {
-            if (i % 2 == 0 && i % 3 == 0) {
-                arr[i] = Math.min(arr[i/2], arr[i/3]) + 1;
-            } else if (i % 2 == 0) {
-                arr[i] = Math.min(arr[i-1], arr[i/2]) + 1;
-            } else if (i % 3 == 0) {
-                arr[i] = Math.min(arr[i-1], arr[i/3]) + 1;
-            } else {
-                arr[i] = arr[i-1] + 1;
-            }
+
+        long[] arr = new long[n+1];
+        arr[n] = 0;
+
+        for (int i = n-1; i > 0; i--) {
+            long one = (i * 3 <= n) ? arr[i * 3] + 1 : Long.MAX_VALUE;
+            long two = (i * 2 <= n) ? arr[i * 2] + 1: Long.MAX_VALUE;
+            long three = arr[i+1] + 1;
+
+            long result = Math.min(one, Math.min(two, three));
+
+            arr[i] = result;
         }
-        System.out.println(arr[n]);
+
+        System.out.println(arr[1]);
     }
 }
