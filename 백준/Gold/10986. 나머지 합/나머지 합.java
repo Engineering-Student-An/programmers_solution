853d122e0483ae9bf5ad70afe39c05f6c06963e7
Engineering-Student-An <chm20060@gmail.com>
2025-03-03 23:15:03 +0900

    [Gold III] Title: 나머지 합, Time: 568 ms, Memory: 171512 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -1,35 +1,47 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner scanner = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        int[] mod = new int[n];
+        long[] arr = new long[n+1];
+        long[] sum = new long[n+1];
+        int[] count = new int[m];
 
-        mod[0] = scanner.nextInt() % m;
-        for (int i = 1; i < n; i++) {
-            mod[i] = (mod[i-1] + scanner.nextInt()) % m;
+        st = new StringTokenizer(br.readLine());
+        count[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Long.parseLong(st.nextToken());
+            sum[i] = ((sum[i-1] % m) + (arr[i] % m)) % m;
+
+            count[(int) sum[i]] ++;
         }
 
-        int[] cnt = new int[m];
-        cnt[0] = 1;
-        for (int i = 0; i < n; i++) {
-            cnt[mod[i]] ++;
+        long[][] combination = new long[1000002][3];
+        for (int i = 0; i <= 1000001; i++) {
+            combination[i][0] = 1;
+        }
+        for (int i = 1; i <= 1000001; i++) {
+            for (int j = 1; j < 3; j++) {
+                combination[i][j] = combination[i-1][j] + combination[i-1][j-1];
+            }
         }
 
         long result = 0;
         for (int i = 0; i < m; i++) {
-            if(cnt[i] > 1) {
-                result += ((long) cnt[i] * (cnt[i]-1)) / 2;
+            if(count[i] != 0) {
+                result += combination[count[i]][2];
             }
         }
 
         System.out.println(result);
     }
 }
-
-
