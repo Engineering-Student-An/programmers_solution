bb770623f7643854045af635d5788d9a6b3fbe3f
Engineering-Student-An <chm20060@gmail.com>
2025-03-08 23:08:17 +0900

    [Silver I] Title: 절댓값 힙, Time: 280 ms, Memory: 25456 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,40 +1,46 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int n = Integer.parseInt(br.readLine());
 
-        PriorityQueue<Integer> queue = new PriorityQueue<>( (o1, o2) -> {
-            int first_abs = Math.abs(o1);
-            int second_abs = Math.abs(o2);
-            if(first_abs == second_abs) {
-                return o1 > o2 ? 1: -1;
-            } else {
-                return first_abs - second_abs;
-            }
-        });
-
+        PriorityQueue<Long> queue = new PriorityQueue<>(new QComparator());
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            int now = Integer.parseInt(br.readLine());
-            if(now!=0) {
-                queue.add(now);
-            } else {
+            long m = Long.parseLong(br.readLine());
+
+            if(m == 0) {
                 if(queue.isEmpty()) {
-                    bw.write(0 + "\n");
+                    sb.append("0\n");
                 } else {
-                    bw.write(queue.poll() + "\n");
+                    long poll = queue.poll();
+                    sb.append(poll).append("\n");
                 }
+            } else {
+                queue.add(m);
             }
         }
 
-        bw.flush();
-        bw.close();
+        System.out.print(sb);
     }
 
+    static class QComparator implements Comparator<Long> {
+        @Override
+        public int compare(Long o1, Long o2) {
+            long abs1 = Math.abs(o1);
+            long abs2 = Math.abs(o2);
+
+            if(abs1 == abs2) {
+                return Long.compare(o1, o2);
+            }
+
+            return Long.compare(abs1, abs2);
+        }
+    }
 }
