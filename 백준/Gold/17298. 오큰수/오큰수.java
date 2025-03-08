3a124f67f10e3d627a9dc9b1fb0d103554da5ecc
Engineering-Student-An <chm20060@gmail.com>
2025-03-08 22:45:48 +0900

    [Gold IV] Title: 오큰수, Time: 880 ms, Memory: 155424 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,4 +1,6 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -6,43 +8,44 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int n = Integer.parseInt(st.nextToken());
-
-        st = new StringTokenizer(br.readLine());
-
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        Stack<Data> stack = new Stack<>();
         int[] result = new int[n];
-
+        Stack<Info> stack = new Stack<>();
         for (int i = 0; i < n; i++) {
 
-            while(!stack.isEmpty() && stack.peek().value < arr[i]) {
-                Data pop = stack.pop();
-                result[pop.index] = arr[i];
+            int now = arr[i];
+
+            while(!stack.isEmpty() && stack.peek().value < now) {
+                Info info = stack.pop();
+                result[info.index] = now;
             }
-            stack.push(new Data(arr[i], i));
+
+            stack.push(new Info(i, now));
         }
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            sb.append((result[i] == 0 ? -1 : result[i])).append(" ");
+            sb.append((result[i] == 0) ? -1 : result[i]).append(" ");
         }
-        System.out.print(sb);
+
+        System.out.println(sb);
     }
 
-    static class Data {
-        int value;
+    static class Info {
         int index;
+        int value;
 
-        public Data(int value, int index) {
-            this.value = value;
+        public Info(int index, int value) {
             this.index = index;
+            this.value = value;
         }
     }
 }
