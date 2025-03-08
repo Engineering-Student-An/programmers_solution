9a3123c0f2a8c73a5e78c04a3e70f8d21ddc65e9
Engineering-Student-An <chm20060@gmail.com>
2025-03-08 15:47:34 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2076 ms, Memory: 671576 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -1,4 +1,6 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
@@ -7,47 +9,46 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
         int l = Integer.parseInt(st.nextToken());
 
+        Deque<Info> deque = new LinkedList<>();
         st = new StringTokenizer(br.readLine());
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        Deque<Data> deque = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            Data data = new Data(arr[i], i);
+        StringBuilder sb = new StringBuilder();
+        deque.add(new Info(0, arr[0]));
+        sb.append(deque.getFirst().value).append(" ");
 
-            while(!deque.isEmpty() && deque.getLast().value > data.value) {
+        for (int i = 1; i < n; i++) {
+
+            while(!deque.isEmpty() && deque.getLast().value > arr[i]) {
                 deque.removeLast();
             }
-            deque.addLast(data);
+            deque.addLast(new Info(i, arr[i]));
 
-            if(i-deque.getFirst().index >= l) {
+            if(i - deque.getFirst().index >= l) {
                 deque.removeFirst();
             }
 
-            bw.write(deque.getFirst().value + " ");
+            sb.append(deque.getFirst().value).append(" ");
         }
 
-        bw.flush();
-        bw.close();
-        System.out.println(bw);
+        System.out.print(sb);
     }
 
-    static class Data {
-        int value;
+    static class Info {
         int index;
+        int value;
 
-        public Data(int value, int index) {
-            this.value = value;
+        public Info(int index, int value) {
             this.index = index;
+            this.value = value;
         }
     }
 }
