6972059ed3146dab3977b7d67be3b1d682f760bd
Engineering-Student-An <chm20060@gmail.com>
2025-03-08 22:07:57 +0900

    [Silver II] Title: 스택 수열, Time: 308 ms, Memory: 27104 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,39 +1,41 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
 
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
-        int next = 0;
+        int index = 1;
+
         StringBuilder sb = new StringBuilder();
         Stack<Integer> stack = new Stack<>();
-        int plus = 0;
-        int minus = 0;
-        for (int i = 1; i <= n; i++) {
-            stack.push(i);
-            sb.append("+").append("\n");
-            plus ++;
+        for (int i = 0; i < n; i++) {
 
-            while (!stack.isEmpty() && next < n) {
-                if(arr[next] == stack.peek()) {
-                    minus++;
-                    sb.append("-").append("\n");
-                    stack.pop();
-                    next++;
-                } else {
-                    break;
-                }
+            while(arr[i] >= index) {
+                stack.push(index ++);
+                sb.append("+").append("\n");
+            }
+
+            if(!stack.isEmpty() && arr[i] == stack.peek()) {
+                stack.pop();
+                sb.append("-").append("\n");
+            } else {
+                sb = new StringBuilder();
+                sb.append("NO");
+                break;
             }
         }
 
-        System.out.print((plus == minus) ? sb : "NO");
+        System.out.print(sb);
     }
 }
