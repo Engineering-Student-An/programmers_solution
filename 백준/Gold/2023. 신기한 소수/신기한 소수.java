a110c94cd139943e46395e934e3db631d9f740d5
Engineering-Student-An <chm20060@gmail.com>
2025-03-16 23:20:40 +0900

    [Gold V] Title: 신기한 소수, Time: 176 ms, Memory: 17752 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -2,49 +2,47 @@ import java.util.Scanner;
 
 public class Main {
 
-    static StringBuilder sb;
     static int n;
-    static int[] nextNum = {1,3,5,7,9};
+    static StringBuilder sb = new StringBuilder();
+    static int[] nextPrime = {1, 3, 7, 9};
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
         n = scanner.nextInt();
-        sb = new StringBuilder();
-        int[] first = {2, 3, 5, 7};
 
-        for (int i = 0; i < 4; i++) {
-            int cnt = 1;
-            nextCheck(first[i], cnt);
+        for (int i = 2; i < 10; i++) {
+            if(isPrime(i)) {
+                dfs(i, 1);
+            }
         }
 
         System.out.print(sb);
     }
 
-    private static void nextCheck(int num, int cnt) {
-        if(!isPrime(num)) return;
-
-        if(cnt == n) {
+    static void dfs(int num, int jarisu) {
+        if(jarisu == n) {
             sb.append(num).append("\n");
             return;
         }
 
-        for (int i = 0; i < 5; i++) {
-            String n = String.valueOf(num) + String.valueOf(nextNum[i]);
-            nextCheck(Integer.parseInt(n), cnt+1);
+        int next = num * 10;
+        for (int i = 0; i < 4; i++) {
+            int nextNum = next + nextPrime[i];
+
+            if(isPrime(nextNum)) {
+                dfs(nextNum, jarisu + 1);
+            }
         }
+
     }
 
-    private static boolean isPrime(int num) {
-        int sqrt = (int) Math.sqrt(num);
-        if(sqrt*sqrt == num) {
-            return false;
-        }
+    static boolean isPrime(int num) {
 
-        for (int i = 2; i <= sqrt; i++) {
-            if(num % i == 0) {
-                return false;
-            }
+        if(Math.sqrt(num) == (int) Math.sqrt(num)) return false;
+
+        for (int i = 2; i < Math.sqrt(num); i++) {
+            if(num % i == 0) return false;
         }
 
         return true;
