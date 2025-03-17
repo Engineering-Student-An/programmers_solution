7056791c14df85f51f8c836cdbf5596ec988aaa2
Engineering-Student-An <chm20060@gmail.com>
2025-03-17 15:12:23 +0900

    [Silver II] Title: DFS와 BFS, Time: 220 ms, Memory: 20248 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -4,9 +4,12 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static PriorityQueue<Integer>[] adjacentList1;
-    static PriorityQueue<Integer>[] adjacentList2;
+
+    static PriorityQueue<Integer>[] adjacencyList1;
+    static PriorityQueue<Integer>[] adjacencyList2;
+    static boolean[] visit;
     static StringBuilder sb;
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,65 +19,63 @@ public class Main {
         int m = Integer.parseInt(st.nextToken());
         int v = Integer.parseInt(st.nextToken());
 
-        adjacentList1 = new PriorityQueue[n+1];
-        adjacentList2 = new PriorityQueue[n+1];
+        adjacencyList1 = new PriorityQueue[n+1];
+        adjacencyList2 = new PriorityQueue[n+1];
         for (int i = 1; i <= n; i++) {
-            adjacentList1[i] = new PriorityQueue<>();
-            adjacentList2[i] = new PriorityQueue<>();
+            adjacencyList1[i] = new PriorityQueue<>();
+            adjacencyList2[i] = new PriorityQueue<>();
         }
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
 
-            adjacentList1[a].add(b);
-            adjacentList1[b].add(a);
-            adjacentList2[a].add(b);
-            adjacentList2[b].add(a);
+            adjacencyList1[start].add(end);
+            adjacencyList2[start].add(end);
+            adjacencyList1[end].add(start);
+            adjacencyList2[end].add(start);
         }
+
         sb = new StringBuilder();
+        visit = new boolean[n + 1];
+        dfs(v);
 
-        boolean[] check = new boolean[n+1];
-        dfs(v, check);
         sb.append("\n");
-
-        check = new boolean[n+1];
-        bfs(v, check);
+        visit = new boolean[n + 1];
+        bfs(v);
 
         System.out.print(sb);
     }
 
-    private static void bfs(int v, boolean[] check) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(v);
-        check[v] = true;
+    static void dfs(int node) {
 
-        while(!queue.isEmpty()) {
+        visit[node] = true;
+        sb.append(node).append(" ");
+        while(!adjacencyList1[node].isEmpty()) {
+            Integer integer = adjacencyList1[node].poll();
+            if(!visit[integer]) {
+                dfs(integer);
+            }
+        }
+    }
+
+    static void bfs(int node) {
+        Queue<Integer> queue = new LinkedList<>();
+        visit[node] = true;
+        queue.add(node);
+
+        while (!queue.isEmpty()) {
             Integer poll = queue.poll();
             sb.append(poll).append(" ");
+            while (!adjacencyList2[poll].isEmpty()) {
+                Integer integer = adjacencyList2[poll].poll();
 
-            while(!adjacentList2[poll].isEmpty()) {
-                Integer integer = adjacentList2[poll].poll();
-                if(!check[integer]) {
+                if(!visit[integer]) {
+                    visit[integer] = true;
                     queue.add(integer);
-                    check[integer] = true;
                 }
             }
         }
     }
-
-    private static void dfs(int v, boolean[] check) {
-        sb.append(v).append(" ");
-        check[v] = true;
-
-        while(!adjacentList1[v].isEmpty()) {
-            Integer poll = adjacentList1[v].poll();
-            if(!check[poll]) {
-                dfs(poll, check);
-            }
-        }
-    }
-
-
 }
