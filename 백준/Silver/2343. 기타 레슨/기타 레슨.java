086bcbf434f5b831ec6a021840d908d812b1158a
Engineering-Student-An <chm20060@gmail.com>
2025-03-17 22:40:11 +0900

    [Silver I] Title: 기타 레슨, Time: 300 ms, Memory: 23896 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.java
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -4,57 +4,54 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n;
-    static int m;
+
+    static int n, m;
+    static int[] arr;
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[n];
-
+        arr = new int[n];
+        int left = 0;
+        int right = 0;
         st = new StringTokenizer(br.readLine());
-        int start = 0;
-        int end = 0;
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-            if(arr[i] > start) start = arr[i];
-            end += arr[i];
+            left = Math.max(left, arr[i]);
+            right += arr[i];
         }
 
-        System.out.println(findMin(arr, start, end));
+        int middle;
+        while(left <= right) {
+            middle = (left + right) / 2;
+
+            if(isPossible(middle)) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+
+        System.out.println(left);
     }
 
-    private static int findMin(int[] arr, int start, int end) {
+    static boolean isPossible(int num) {
 
-        while(start<=end) {
-
-            int middle = (start + end) / 2;
-            int count = 0;
-            int sum = 0;
-
-            for (int i = 0; i < n; i++) {
-                if(sum + arr[i] > middle) {
-                    sum = 0;
-                    count ++;
-                }
-
-                if(count > m) break;
-
-                sum += arr[i];
+        int sum = 0;
+        int count = 1;
+        for (int i = 0; i < n; i++) {
+            if(sum + arr[i] > num) {
+                sum = 0;
+                count ++;
             }
-            count++;
 
-            if(count > m) {
-                start = middle + 1;
-            } else {
-                end = middle - 1;
-            }
+            sum += arr[i];
         }
 
-        return start;
+        return (count <= m);
     }
 }
