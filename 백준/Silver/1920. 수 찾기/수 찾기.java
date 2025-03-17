ca7062b4b4aa538705d6a5c37c9752059b6028fc
Engineering-Student-An <chm20060@gmail.com>
2025-03-17 22:13:54 +0900

    [Silver IV] Title: 수 찾기, Time: 628 ms, Memory: 43088 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -5,48 +5,54 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static long[] arr;
+
+    static int n;
+    static int[] arr;
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int n = Integer.parseInt(st.nextToken());
-
-        arr = new long[n];
-        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            arr[i] = Long.parseLong(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr);
 
-        st = new StringTokenizer(br.readLine());
-        int m = Integer.parseInt(st.nextToken());
-
+        int t = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < m; i++) {
-            long num = Long.parseLong(st.nextToken());
+        for (int i = 0; i < t; i++) {
+            int find = Integer.parseInt(st.nextToken());
 
-            sb.append( (findNum(num, 0, n-1) ? 1 : 0)).append("\n");
+            sb.append(isExist(find)).append("\n");
         }
 
         System.out.print(sb);
     }
 
-    private static boolean findNum(long num, int start, int end) {
+    static int isExist(int num) {
 
-        while(start <= end) {
-            int middle = (start + end) / 2;
-            if(arr[middle] == num) {
-                return true;
-            } else if(arr[middle] > num) {
-                end = middle - 1;
+        int left = 0;
+        int right = n-1;
+        int middle = 0;
+
+        while (left <= right) {
+            middle = (left + right) / 2;
+
+            if(arr[middle] == num) return 1;
+
+            else if(arr[middle] > num) {
+                right = middle - 1;
             } else {
-                start = middle + 1;
+                left = middle + 1;
             }
         }
-        return false;
+
+        return (arr[middle] == num) ? 1 : 0;
     }
 }
