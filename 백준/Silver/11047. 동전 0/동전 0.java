37fd134115a7f348f15dac8c7ead1e7fc2694040
Engineering-Student-An <chm20060@gmail.com>
2025-03-18 23:03:16 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,28 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner scanner = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = scanner.nextInt();
-        int k = scanner.nextInt();
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
         int[] arr = new int[n];
-
         for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
-        int count = 0;
-
+        long result = 0;
         for (int i = n-1; i >= 0; i--) {
-
-            count += k/arr[i];
-
-            k%=arr[i];
+            result += (k / arr[i]);
+            k %= arr[i];
         }
 
-        System.out.println(count);
+        System.out.println(result);
     }
 }
