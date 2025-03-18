197bac2d5993da0b0504fa24814441b0fc115fe6
Engineering-Student-An <chm20060@gmail.com>
2025-03-19 00:05:25 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 336 ms, Memory: 25836 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -2,33 +2,33 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
 
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        PriorityQueue<Long> queue = new PriorityQueue<>();
 
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            queue.add(Integer.parseInt(st.nextToken()));
-        }
-        if(n == 1) {
-            System.out.println(0);
-        } else {
-            long ans = 0;
-            while(queue.size() > 1) {
-                int next = queue.poll() + queue.poll();
-                ans += next;
-                queue.add(next);
-            }
-            System.out.println(ans);
+            queue.add(Long.parseLong(br.readLine()));
         }
 
+        long result = 0;
+        while (!queue.isEmpty() && queue.size() > 1) {
+            Long first = queue.poll();
+            result += first;
+            if(queue.isEmpty()) break;
+
+            Long second = queue.poll();
+            result += second;
+            if(queue.isEmpty()) break;
+
+            queue.add(first + second);
+        }
+
+        System.out.println(result);
     }
 }
