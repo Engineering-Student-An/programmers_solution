bdfa28901471310a3dd2288ec619cd3816e84966
Engineering-Student-An <chm20060@gmail.com>
2025-03-20 18:58:31 +0900

    [Gold IV] Title: 물통, Time: 172 ms, Memory: 17736 KB -BaekjoonHub

--- 백준/Gold/2251. 물통/물통.java
+++ 백준/Gold/2251. 물통/물통.java
@@ -5,87 +5,92 @@ import java.util.Scanner;
 public class Main {
 
     static boolean[][] visit;
-    static Water[] waters;
-    static int[] cap;
+    static Move[] moves;
+    static StringBuilder sb;
+    static int[] possible = new int[3];
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        cap = new int[3];
+        possible[0] = scanner.nextInt();
+        possible[1] = scanner.nextInt();
+        possible[2] = scanner.nextInt();
+
+        visit = new boolean[possible[0]+1][possible[2]+1];
+
+        moves = new Move[6];
+        int index = 0;
         for (int i = 0; i < 3; i++) {
-            cap[i] = scanner.nextInt();
+            for (int j = 0; j < 3; j++) {
+                if(i != j) {
+                    moves[index ++] = new Move(i, j);
+                }
+            }
         }
 
-        // 물 이동 경로 전체 경우
-        waters = new Water[6];
-        waters[0] = new Water(0, 1);
-        waters[1] = new Water(0, 2);
-        waters[2] = new Water(1, 0);
-        waters[3] = new Water(1, 2);
-        waters[4] = new Water(2, 0);
-        waters[5] = new Water(2, 1);
-
-        int[] now = {0, 0, cap[2]};
-
-        visit = new boolean[cap[0]+1][cap[2]+1];    // a, c
-
-        bfs(now);
+        sb = new StringBuilder();
+        bfs();
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i <= cap[2]; i++) {
-            if(visit[0][i]) sb.append(i).append(" ");
+        for (int i = 0; i <= possible[2]; i++) {
+            if(visit[0][i]) {
+                sb.append(i).append(" ");
+            }
         }
 
-        System.out.print(sb);
+        System.out.println(sb);
     }
 
-    public static void bfs(int[] now) {
-
+    static void bfs() {
+        // a , c
+        visit[0][possible[2]] = true;
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{now[0], now[1], now[2]});
-
-        visit[now[0]][now[2]] = true;
+        queue.add(new int[]{0, 0, possible[2]});
 
         while (!queue.isEmpty()) {
             int[] poll = queue.poll();
-            now = new int[] {poll[0], poll[1], poll[2]};
 
             for (int i = 0; i < 6; i++) {
-                Water water = waters[i];
 
-                int[] temp = new int[3];
-                for (int j = 0; j < 3; j++) {
-                    temp[j] = now[j];
+                int[] temp = {poll[0], poll[1], poll[2]};
+                int start = temp[moves[i].start];
+                int end = temp[moves[i].end];
+
+                if(start <= 0) continue;
+
+                int difference = Math.min(possible[moves[i].end] - end, start);
+
+                temp[moves[i].start] -= difference;
+                temp[moves[i].end] += difference;
+
+                if(!visit[temp[0]][temp[2]]) {
+                    visit[temp[0]][temp[2]] = true;
+                    queue.add(new int[] {temp[0], temp[1], temp[2]});
                 }
-                if(temp[water.sender] > 0) {
-                    temp[water.receiver] += temp[water.sender];
-                    temp[water.sender] = 0;
-                    int nam = temp[water.receiver] - cap[water.receiver];
-
-                    if(nam > 0) {
-                        temp[water.receiver] -= nam;
-                        temp[water.sender] += nam;
-                    }
-                    if(!visit[temp[0]][temp[2]]) {
-                        queue.add(new int[]{temp[0], temp[1], temp[2]});
-                        visit[temp[0]][temp[2]] = true;
-                    }
-                }
-
-
             }
         }
     }
 
-    static class Water {
+    static class Now {
+        int a;
+        int b;
+        int c;
 
-        int sender;
-        int receiver;
+        public Now (int a, int b, int c) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+    }
 
-        public Water(int sender, int receiver) {
-            this.sender = sender;
-            this.receiver = receiver;
+    static class Move {
+        int start;
+        int end;
+
+        public Move(int start, int end) {
+            this.start = start;
+            this.end = end;
         }
     }
 }
