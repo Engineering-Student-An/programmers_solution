1f85439ae2840765232b27263d3466b028103de3
Engineering-Student-An <chm20060@gmail.com>
2025-03-31 17:39:50 +0900

    [Gold IV] Title: 플로이드, Time: 400 ms, Memory: 42632 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -4,48 +4,57 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    static int n, m;
+    static long[][] result;
+
     public static void main(String[] args) throws IOException {
 
-        final long INF = Integer.MAX_VALUE;
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
-        int e = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
 
-        long[][] arr = new long[n+1][n+1];
+        result = new long[n+1][n+1];
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                if(i!=j) arr[i][j] = INF;
+                result[i][j] = (i == j) ? 0 : Long.MAX_VALUE;
             }
         }
 
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < e; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int value = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            long value = Long.parseLong(st.nextToken());
 
-            arr[start][end] = Math.min(value, arr[start][end]);
+            result[s][e] = Math.min(result[s][e], value);
         }
+
+        floydWarshall();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                sb.append(result[i][j] != Long.MAX_VALUE ? result[i][j] : 0).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.print(sb);
+    }
+
+    static void floydWarshall() {
 
         for (int k = 1; k <= n; k++) {
-            for (int start = 1; start <= n; start++) {
-                for (int end = 1; end <= n; end++) {
-                    arr[start][end] = Math.min(arr[start][end], arr[start][k] + arr[k][end]);
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if(result[i][k] != Long.MAX_VALUE && result[k][j] != Long.MAX_VALUE) {
+                        result[i][j] = Math.min(result[i][j], result[i][k] + result[k][j]);
+                    }
                 }
             }
-        }
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                System.out.print((arr[i][j] != INF) ? arr[i][j] : "0");
-                System.out.print(" ");
-            }
-            System.out.println();
         }
     }
 }
