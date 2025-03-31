42c1147bd5bb328a7514c13daaaea5c4df758e4a
Engineering-Student-An <chm20060@gmail.com>
2025-03-31 18:58:37 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 116 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -4,52 +4,61 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    static int n, m;
+    static long[][] result;
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        int n = Integer.parseInt(st.nextToken());
-        int e = Integer.parseInt(st.nextToken());
-
-        long[][] arr = new long[n+1][n+1];
+        result = new long[n+1][n+1];
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                if(i!=j) arr[i][j] = Integer.MAX_VALUE;
+                result[i][j] = Long.MAX_VALUE;
             }
         }
 
-        for (int i = 0; i < e; i++) {
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int v1 = Integer.parseInt(st.nextToken());
-            int v2 = Integer.parseInt(st.nextToken());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
 
-            arr[v1][v2] = 1;
-            arr[v2][v1] = 1;
+            result[u][v] = 1;
+            result[v][u] = 1;
         }
 
-        for (int k = 1; k <= n; k++) {
-            for (int start = 1; start <= n; start++) {
-                for (int end = 1; end <= n; end++) {
-                    arr[start][end] = Math.min(arr[start][end], arr[start][k] + arr[k][end]);
-                }
-            }
-        }
+        floydWarshall();
 
-        long min = Integer.MAX_VALUE;
-        int minInd = 0;
+        int ans = 1;
+        long max = Long.MAX_VALUE;
         for (int i = 1; i <= n; i++) {
             long sum = 0;
             for (int j = 1; j <= n; j++) {
-                sum += arr[i][j];
+                if(result[i][j] != Long.MAX_VALUE) sum += result[i][j];
             }
-
-            if(sum < min) {
-                min = sum;
-                minInd = i;
+            if(max > sum) {
+                max = sum;
+                ans = i;
             }
         }
 
-        System.out.println(minInd);
+        System.out.print(ans);
+    }
+
+    static void floydWarshall() {
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if(i!=j && result[i][k] != Long.MAX_VALUE && result[k][j] != Long.MAX_VALUE) {
+                        result[i][j] = Math.min(result[i][j], result[i][k] + result[k][j]);
+                    }
+                }
+            }
+        }
     }
 }
