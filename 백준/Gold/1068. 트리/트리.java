8fdf0fdcfae9dca28eaa565cdf2d53a54926f0be
Engineering-Student-An <chm20060@gmail.com>
2025-04-06 13:23:04 +0900

    [Gold V] Title: 트리, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -1,60 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static List<Integer>[] adjacencyList;
-    static int[] arr;
-    static boolean[] visit;
-    static int result;
+    static int n, remove;
+    static ArrayList<Integer>[] adjacencyList;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
 
         adjacencyList = new ArrayList[n];
         for (int i = 0; i < n; i++) {
             adjacencyList[i] = new ArrayList<>();
         }
 
-        arr = new int[n];
-        visit = new boolean[n];
-        int parent = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int root = 0;
         for (int i = 0; i < n; i++) {
-            int num = scanner.nextInt();
-            arr[i] = num;
-            if(num >= 0) {
-                adjacencyList[num].add(i);
+            int n = Integer.parseInt(st.nextToken());
+            if(n != -1) {
+                adjacencyList[n].add(i);
             } else {
-                parent = i;
+                root = i;
             }
         }
-        int target = scanner.nextInt();
 
-        dfs(parent, target);
+        remove = Integer.parseInt(br.readLine());
 
-        System.out.println(result);
+        System.out.println(bfs(root));
     }
 
-    static void dfs(int node, int target) {
+    static int bfs(int num) {
 
-        if(node == target) {
-            return;
-        }
-        boolean check = false;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(num);
+        if(num == remove) return 0;
 
-        for (Integer i : adjacencyList[node]) {
-            if(i != target && !visit[i]) {
-                visit[i] = true;
-                dfs(i, target);
-                check = true;
+        int result = 0;
+        while (!queue.isEmpty()) {
+            Integer now = queue.poll();
+
+            boolean check = false;
+            for(Integer i : adjacencyList[now]) {
+                if(i != remove) {
+                    queue.add(i);
+                    check = true;
+                }
             }
+
+            if(!check) result ++;
         }
 
-        if(!check) {
-            result ++;
-        }
+        return result;
     }
 }
