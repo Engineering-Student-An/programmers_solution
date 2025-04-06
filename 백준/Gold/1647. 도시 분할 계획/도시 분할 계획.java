e0afbc93824d4f7c1d6e84eba8cf86c57ccd77cd
Engineering-Student-An <chm20060@gmail.com>
2025-04-06 11:39:11 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1204 ms, Memory: 332452 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m;
+    static PriorityQueue<Edge> edges = new PriorityQueue<>((o1, o2) -> Long.compare(o1.v, o2.v));
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            long v = Integer.parseInt(st.nextToken());
+
+            edges.add(new Edge(s, e, v));
+        }
+
+        parent = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        int count = 0;
+        long sum = 0;
+        while (count < n - 2) {
+            Edge edge = edges.poll();
+
+            if(find(edge.s) != find(edge.e)) {
+                union(edge.s, edge.e);
+                count ++;
+                sum += edge.v;
+            }
+        }
+
+        System.out.println(sum);
+    }
+
+    static int find(int a) {
+        if(parent[a] == a) return a;
+
+        return parent[a] = find(parent[a]);
+    }
+
+    static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if(a != b) parent[b] = a;
+    }
+
+    static class Edge {
+        int s;
+        int e;
+        long v;
+
+        public Edge(int s, int e, long v) {
+            this.s = s;
+            this.e = e;
+            this.v = v;
+        }
+    }
+}
