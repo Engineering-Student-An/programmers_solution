9cb135d0fbf6978d95c83cc902cb29065f3b54cb
Engineering-Student-An <chm20060@gmail.com>
2025-04-23 15:24:49 +0900

    [Bronze I] Title: 이항 계수 1, Time: 176 ms, Memory: 17720 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -8,19 +8,16 @@ public class Main {
         int n = scanner.nextInt();
         int k = scanner.nextInt();
 
-        int[][] arr = new int[n+1][k+1];
-
-        for (int i = 0; i < n + 1; i++) {
+        long[][] arr = new long[n+1][n+1];
+        for (int i = 0; i <= n; i++) {
             arr[i][0] = 1;
         }
 
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < k + 1; j++) {
-
-                arr[i][j] = arr[i-1][j] + arr[i-1][j-1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
             }
         }
-
         System.out.println(arr[n][k]);
     }
 }
