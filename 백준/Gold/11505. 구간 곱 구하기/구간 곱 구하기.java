5c059226641269bcdaea2fdbdd46a127ad501c96
Engineering-Student-An <chm20060@gmail.com>
2025-04-24 16:45:07 +0900

    [Gold I] Title: 구간 곱 구하기, Time: 460 ms, Memory: 87096 KB -BaekjoonHub

--- 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.java
+++ 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.java
@@ -5,6 +5,9 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+    static int n, m, c;
+    static int k = 0;
+    static int size;
     static long[] segmentTree;
 
     public static void main(String[] args) throws IOException {
@@ -12,74 +15,71 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int s = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
 
-        int k = 0;
-        while (Math.pow(2, k) < n) {
+        while (Math.pow(2, k) <= n) {
             k++;
         }
 
-        int fullIndex = (int) (Math.pow(2, k) * 2);
-        int dataStartIndex = (int) Math.pow(2, k);
-        int temp = dataStartIndex;
-        segmentTree = new long[fullIndex];
-
-        for (int i = 0; i < n; i++) {
-            segmentTree[temp++] = Long.parseLong(br.readLine());
+        size = (int) (Math.pow(2, k + 1));
+        segmentTree = new long[size];
+        for (int i = size / 2; i < n + size / 2; i++) {
+            segmentTree[i] = Long.parseLong(br.readLine());
         }
 
-        for (int i = dataStartIndex-1; i > 0; i--) {
-            segmentTree[i] = (segmentTree[i*2] % 1000000007) * (segmentTree[i*2 + 1] % 1000000007) % 1000000007;
+        for (int i = size / 2 - 1; i > 0; i--) {
+            segmentTree[i] = ((segmentTree[i * 2] % 1000000007) * (segmentTree[i * 2 + 1] % 1000000007)) % 1000000007;
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < m + s; i++) {
+        for (int i = 0; i < m + c; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            long c = Long.parseLong(st.nextToken());
 
-            if(a == 1) {
-                change(b, c, dataStartIndex);
-            } else if (a == 2) {
-                sb.append(partMul(b, (int) c, dataStartIndex)).append("\n");
+            int a = Integer.parseInt(st.nextToken());
+            if (a == 1) {
+                int b = Integer.parseInt(st.nextToken());
+                long c = Long.parseLong(st.nextToken());
+                change(b, c);
+            } else {
+                int b = Integer.parseInt(st.nextToken());
+                int c = Integer.parseInt(st.nextToken());
+                sb.append(mul(b, c)).append("\n");
             }
         }
+
         System.out.print(sb);
     }
 
-    static long partMul(int b, int c, int dataStartIndex) {
+    static void change(int index, long value) {
+        index += (int) (Math.pow(2, k) - 1);
 
-        b = b + dataStartIndex - 1;
-        c = c + dataStartIndex - 1;
-
-        long result = 1;
-        while(b <= c) {
-            if(b % 2 == 1) {
-                result = result * (segmentTree[b]) % 1000000007;
-            }
-            if(c % 2 == 0) {
-                result = result * (segmentTree[c]) % 1000000007;
-            }
-
-            b = (b + 1) / 2;
-            c = (c - 1) / 2;
+        segmentTree[index] = value;
+        index /= 2;
+        while(index > 0) {
+            segmentTree[index] = ((segmentTree[index * 2] % 1000000007) * (segmentTree[index * 2 + 1] % 1000000007)) % 1000000007;
+            index /= 2;
         }
-
-        return result;
     }
 
-    static void change(int targetIndex, long c, int dataStartIndex) {
+    static long mul(int startIdx, int endIdx) {
+        startIdx += (int) (Math.pow(2, k) - 1);
+        endIdx += (int) (Math.pow(2, k) - 1);
 
-        targetIndex = targetIndex + dataStartIndex - 1;
-        segmentTree[targetIndex] = c;
-        targetIndex /= 2;
+        long result = 1;
+        while (startIdx <= endIdx) {
+            if (startIdx % 2 == 1) {
+                result = result * segmentTree[startIdx] % 1000000007;
+            }
+            if (endIdx % 2 == 0) {
+                result = result * segmentTree[endIdx] % 1000000007;
+            }
 
-        while(targetIndex > 0) {
-            segmentTree[targetIndex] = (segmentTree[targetIndex*2] % 1000000007) * (segmentTree[targetIndex*2 + 1] % 1000000007) % 1000000007;
-            targetIndex /= 2;
+            startIdx = (startIdx + 1) / 2;
+            endIdx = (endIdx - 1) / 2;
         }
+
+        return result % 1000000007;
     }
 }
