e4e3bf16c066081dd0fa6144c0acd1c5b84f81ed
Engineering-Student-An <chm20060@gmail.com>
2025-04-25 15:32:03 +0900

    [Platinum V] Title: LCA 2, Time: 712 ms, Memory: 89420 KB -BaekjoonHub

--- 백준/Platinum/11438. LCA 2/LCA 2.java
+++ 백준/Platinum/11438. LCA 2/LCA 2.java
@@ -1,137 +1,131 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static List<Integer>[] adjacencyList;
-    static int n;
-    static int[][] parent;
+    static int n, k;
+    static ArrayList<Integer>[] adjacencyList;
     static int[] firstParent;
-    static int[] depthList;
-    static int k;
+    static int[][] parent;
+    static int[] depth;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         n = Integer.parseInt(br.readLine());
-
-        adjacencyList = new ArrayList[n+1];
-        for (int i = 0; i < n + 1; i++) {
+        adjacencyList = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
             adjacencyList[i] = new ArrayList<>();
         }
 
-        StringTokenizer st;
-        for (int i = 0; i < n-1; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n - 1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
 
-            adjacencyList[a].add(b);
-            adjacencyList[b].add(a);
+            adjacencyList[u].add(v);
+            adjacencyList[v].add(u);
         }
 
+        depth = new int[n + 1];
         firstParent = new int[n+1];
-        depthList = new int[n+1];
-        int level = depthOfTree();
+        int height = checkDepth();
 
         k = 0;
-        while(level >= Math.pow(2, k)) {
+        while(Math.pow(2, k) < height) {
             k ++;
         }
-        k--;
 
-        parent = new int[k+1][n+1];
-
-        for (int i = 1; i < n + 1; i++) {
+        parent = new int[k][n+1];
+        for (int i = 1; i <= n; i++) {
             parent[0][i] = firstParent[i];
         }
-        for (int i = 1; i <= k; i++) {
-            for (int j = 0; j < n + 1; j++) {
+
+        for (int i = 1; i < k; i++) {
+            for (int j = 1; j <= n; j++) {
                 parent[i][j] = parent[i-1][parent[i-1][j]];
             }
         }
 
-        StringBuilder sb = new StringBuilder();
         int m = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            sb.append(lca(a, b)).append("\n");
+            sb.append(fastLca(a, b)).append("\n");
         }
-
         System.out.print(sb);
     }
 
-    static int lca(int a, int b) {
+    static int fastLca(int a, int b) {
 
-        int depthDifference = depthList[a] - depthList[b];
-        if(depthDifference > 0) {
+        // 깊이 맞추기
+        if(depth[a] > depth[b]) {
             int temp = a;
             a = b;
             b = temp;
         }
-        depthDifference = Math.abs(depthDifference);
 
-        // 깊이 맞추기
-        for (int i = k; i >= 0; i--) {
-            if(depthDifference >= Math.pow(2, i) && depthList[a] <= depthList[parent[i][b]]) {
-                b = parent[i][b];
-                depthDifference = Math.abs(depthList[a] - depthList[b]);
+        int h = k-1;
+        while(depth[a] < depth[b]) {
+            if(depth[a] <= depth[parent[h][b]]) {
+                b = parent[h][b];
             }
+            h--;
         }
 
+        // 2^h 만큼 올라가면서 조상이 겹치는 곳 찾기
+        h = 0;
+        while(h < k && parent[h][a] != parent[h][b]) {
+            h ++;
+        }
+
+        // 최소공통조상 찾기
         if(a == b) {
             return a;
         }
-
-        for (int i = k; i >= 0; i--) {
-            if (parent[i][a] != parent[i][b]) {
+        for (int i = k-1; i >= 0; i--) {
+            if(parent[i][a] != parent[i][b]) {
                 a = parent[i][a];
                 b = parent[i][b];
             }
         }
 
-        return firstParent[a];
+        return parent[0][a];
     }
 
-    static int depthOfTree() {
+    static int checkDepth() {
 
+        int maxDepth = -1;
         boolean[] visit = new boolean[n+1];
-
         visit[1] = true;
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(1, 0));
+        depth[1] = 1;
 
-        int depth = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+
         while (!queue.isEmpty()) {
-            Node poll = queue.poll();
-            depth = Math.max(depth, poll.depth);
+            Integer now = queue.poll();
 
-            for (Integer integer : adjacencyList[poll.value]) {
-                if(!visit[integer]) {
-                    queue.add(new Node(integer, poll.depth + 1));
-                    visit[integer] = true;
-                    firstParent[integer] = poll.value;
-                    depthList[integer] = poll.depth + 1;
+            for (Integer i : adjacencyList[now]) {
+                if(!visit[i]) {
+                    visit[i] = true;
+                    firstParent[i] = now;
+                    queue.add(i);
+                    depth[i] = depth[now] + 1;
+                    maxDepth = Math.max(maxDepth, depth[i]);
                 }
             }
         }
 
-        return depth;
-    }
-
-    static class Node {
-        int value;
-        int depth;
-
-        public Node(int value, int depth) {
-            this.value = value;
-            this.depth = depth;
-        }
+        return maxDepth;
     }
 }
