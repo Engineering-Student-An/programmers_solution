697c67c73a36e862d7df484bd02d21b037bdd1db
Engineering-Student-An <chm20060@gmail.com>
2025-04-25 16:07:35 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 104 ms, Memory: 13984 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -1,25 +1,27 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner scanner = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int t = scanner.nextInt();
-        
-        int[][] arr = new int[15][15];
-        for (int i = 1; i <= 14; i++) {
+        long[][] arr = new long[15][15];
+        for (int i = 0; i < 15; i++) {
             arr[0][i] = i;
         }
 
-        for (int i = 1; i <= 14; i++) {
-            for (int j = 1; j <= 14; j++) {
+        for (int i = 1; i < 15; i++) {
+            for (int j = 1; j < 15; j++) {
                 arr[i][j] = arr[i-1][j] + arr[i][j-1];
             }
         }
-        for (int tc = 0; tc < t; tc++) {
-            int k = scanner.nextInt();
-            int n = scanner.nextInt();
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
 
             System.out.println(arr[k][n]);
         }
