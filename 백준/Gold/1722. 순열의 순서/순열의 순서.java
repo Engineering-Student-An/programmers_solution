c9e572a1c9f63e84462aa1a6ad80c3d886360518
Engineering-Student-An <chm20060@gmail.com>
2025-04-28 21:59:44 +0900

    [Gold V] Title: 순열의 순서, Time: 112 ms, Memory: 14428 KB -BaekjoonHub

--- 백준/Gold/1722. 순열의 순서/순열의 순서.java
+++ 백준/Gold/1722. 순열의 순서/순열의 순서.java
@@ -1,71 +1,73 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
 
-    static boolean[] visit;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
-        
-        Scanner scanner = new Scanner(System.in);
-        
-        int n = scanner.nextInt();
-        visit = new boolean[n+1];
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int Q = scanner.nextInt();
-        StringBuilder sb = new StringBuilder();
-
-        // 미리 초기화
+        boolean[] visit = new boolean[n+1];
         long[] arr = new long[n+1];
         arr[n] = 1;
-        int count = 2;
         for (int i = n-1; i > 0; i--) {
-            arr[i] = arr[i+1] * (count ++);
+            arr[i] = arr[i + 1] * (n - i + 1);
         }
 
-        if(Q == 1) {
-            long k = scanner.nextLong();
+        int q = Integer.parseInt(st.nextToken());
+        if(q == 1) {
+            long k = Long.parseLong(st.nextToken());
+            int[] ans = new int[n+1];
 
-            for (int i = 1; i < n; i++) {
-                for (int cnt = 1; cnt <= n; cnt++) {
-                    if(k <= arr[i+1] * cnt) {
-                        int index = 0;
-                        int check = 0;
-                        while(check < cnt) {
-                            index ++;
-                            if(!visit[index]) {
-                                check ++;
-                            }
-                        }
-                        visit[index] = true;
-                        sb.append(index).append(" ");
-                        k -= (arr[i+1] * (cnt - 1));
+            for (int i = 1; i <= n-1; i++) {
+                int count = 1;
+                for (int j = 1; j <= n; j++) {
+                    if(visit[j]) continue;
+                    if(arr[i+1] * count >= k) {
+                        k -= (arr[i+1] * (count-1));
+                        visit[j] = true;
+                        ans[i] = j;
                         break;
+                    } else {
+                        count ++;
                     }
                 }
             }
-
             for (int i = 1; i <= n; i++) {
-                if(!visit[i]) sb.append(i);
+                if(!visit[i]) {
+                    ans[n] = i;
+                    break;
+                }
             }
-            System.out.print(sb);
-        } else {
-            long result = 1;
-            for (int i = 1; i < n; i++) {
-                int num = scanner.nextInt();
+            for (int i = 1; i <= n; i++) {
+                System.out.print(ans[i] + " ");
+            }
 
-                int cnt = 0;
-                for (int j = 1; j <= num; j++) {
-                    if(!visit[j]) {
-                        cnt ++;
+        } else {
+            int[] p = new int[n+1];
+            for (int i = 1; i <= n; i++) {
+                p[i] = Integer.parseInt(st.nextToken());
+            }
+
+            long ans = 1;
+            for (int i = 1; i <= n-1; i++) {
+                int count = 0;
+                for (int j = 1; j <= n; j++) {
+                    if(visit[j]) continue;
+                    if(p[i] == j) {
+                        ans += count * arr[i+1];
+                        visit[j] = true;
+                        break;
+                    } else {
+                        count ++;
                     }
                 }
-
-                visit[num] = true;
-                result += (arr[i+1] * (cnt - 1));
-
             }
-
-            System.out.println(result);
+            System.out.println(ans);
         }
     }
 }
