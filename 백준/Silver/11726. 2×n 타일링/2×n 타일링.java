36bbf4429c030bbb97056ba92408aed58c83aa7c
Engineering-Student-An <chm20060@gmail.com>
2025-04-30 17:52:50 +0900

    [Silver III] Title: 2×n 타일링, Time: 172 ms, Memory: 17748 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -4,25 +4,15 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
         int n = scanner.nextInt();
 
-        long[][] arr = new long[n+1][n+1];
-        for (int i = 0; i < n + 1; i++) {
-            arr[i][0] = 1;
+        long[] result = new long[n+1];
+        result[1] = 1;
+        if(n > 1) result[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            result[i] = (result[i-2] + result[i-1]) % 10007;
         }
 
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < n + 1; j++) {
-                arr[i][j] = (arr[i-1][j-1] + arr[i-1][j]) % 10007;
-            }
-        }
-
-        long result = 0;
-        for (int i = 0; i < n; i++) {
-            result += arr[n-i][i] % 10007;
-        }
-
-        System.out.println(result % 10007);
+        System.out.println(result[n]);
     }
 }
