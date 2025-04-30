7346302da6c1c512bfcae05a6cbde18a24b886de
Engineering-Student-An <chm20060@gmail.com>
2025-04-30 16:43:11 +0900

    [Gold II] Title: 사전, Time: 104 ms, Memory: 14512 KB -BaekjoonHub

--- 백준/Gold/1256. 사전/사전.java
+++ 백준/Gold/1256. 사전/사전.java
@@ -1,49 +1,51 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        
-        Scanner scanner = new Scanner(System.in);
-        
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        long k = scanner.nextLong();
-        
-        long[][] arr = new long[n+m+1][n+m+1];
+    public static void main(String[] args) throws IOException {
 
-        for (int i = 0; i <= n+m; i++) {
-            arr[i][0] = 1;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        long k = Long.parseLong(st.nextToken());
+
+        long[][] combination = new long[n + m + 1][n + m + 1];
+        for (int i = 0; i < n + m + 1; i++) {
+            combination[i][0] = 1;
         }
 
-        for (int i = 1; i <= n+m; i++) {
-            for (int j = 1; j <= n+m; j++) {
-                if(arr[i-1][j-1] + arr[i-1][j] > 1000000000) {
-                    arr[i][j] = 1000000001;
-                } else {
-                    arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+        for (int i = 1; i <= n + m; i++) {
+            for (int j = 1; j <= n + m; j++) {
+                long next = combination[i - 1][j] + combination[i - 1][j - 1];
+                if(next > 1000000000) {
+                    next = 1000000001;
                 }
+                combination[i][j] = next;
             }
         }
 
-        if(k <= arr[n+m][n]) {
+        if (k > combination[n + m][m]) System.out.println(-1);
+        else {
             StringBuilder sb = new StringBuilder();
-            int a = n;
-            for (int i = 1; i <= n + m; i++) {
-                // a 선택 가능한지
-                if (a > 0 && arr[n + m - i][a - 1] >= k) {
-                    sb.append("a");
-                    a--;
-                }
-                // a 선택 불가능한지
-                else {
+            int size = n + m;
+            for (int i = 0; i < size; i++) {
+
+                long next = combination[n + m - 1][m];
+                if (k <= next) {
+                   sb.append("a");
+                    n--;
+                } else {
+                    k -= next;
                     sb.append("z");
-                    if (a > 0) k -= arr[n + m - i][a - 1];
+                    m--;
                 }
             }
 
-            System.out.print(sb);
-        } else {
-            System.out.println(-1);
+            System.out.println(sb);
         }
     }
 }
