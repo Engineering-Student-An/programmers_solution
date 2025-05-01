693b2936bd19684ff4977f2ec220b480a1c713cf
Engineering-Student-An <chm20060@gmail.com>
2025-05-01 16:40:45 +0900

    [Gold V] Title: LCS, Time: 144 ms, Memory: 18640 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,27 +1,26 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner scanner = new Scanner(System.in);
-        String first = scanner.next();
-        String second = scanner.next();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String first = br.readLine();
+        String second = br.readLine();
 
-        int[][] arr = new int[first.length() + 1][second.length() + 1];
+        int[][] lcs = new int[second.length()+1][first.length()+1];
+//        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= second.length(); i++) {
+            for (int j = 1; j <= first.length(); j++) {
+                char f = first.charAt(j - 1);
+                char s = second.charAt(i - 1);
 
-        for (int i = 0; i <= first.length(); i++) {
-            for (int j = 0; j <= second.length(); j++) {
-                if(i == 0 || j == 0) {
-                    arr[i][j] = 0;
-                }
-                else if(first.charAt(i-1) != second.charAt(j-1)) {
-                    arr[i][j] = Math.max(arr[i-1][j], arr[i][j-1]);
-                } else {
-                    arr[i][j] = arr[i-1][j-1] + 1;
-                }
+                if(f!=s) lcs[i][j] = Math.max(lcs[i-1][j], lcs[i][j-1]);
+                else lcs[i][j] = lcs[i-1][j-1] + 1;
+                
             }
         }
-
-        System.out.println(arr[first.length()][second.length()]);
+        System.out.println(lcs[second.length()][first.length()]);
     }
 }
