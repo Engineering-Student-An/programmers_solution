e1fc22b38b78b8ef895fb04a4e7bba55956ab4c3
Engineering-Student-An <chm20060@gmail.com>
2025-05-01 15:54:21 +0900

    [Gold V] Title: 연속합 2, Time: 264 ms, Memory: 26284 KB -BaekjoonHub

--- 백준/Gold/13398. 연속합 2/연속합 2.java
+++ 백준/Gold/13398. 연속합 2/연속합 2.java
@@ -5,39 +5,32 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[n];
+        int[] arr = new int[n+1];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        long[] left = new long[n];
-        long[] right = new long[n];
-
-        left[0] = arr[0];
-        for (int i = 1; i < n; i++) {
+        long[] left = new long[n+2];
+        long[] right = new long[n+2];
+        for (int i = 1; i <= n; i++) {
             left[i] = Math.max(arr[i], left[i-1] + arr[i]);
         }
-
-        right[n-1] = arr[n-1];
-        for (int i = n-2; i >= 0; i--) {
+        for (int i = n; i > 0; i--) {
             right[i] = Math.max(arr[i], right[i+1] + arr[i]);
         }
 
-        long result = left[0];
-        for (int i = 0; i < n; i++) {
-            result = Math.max(result, Math.max(left[i], right[i]));
+        long max = Integer.MIN_VALUE;
+        for (int i = 2; i < n; i++) {
+            max = Math.max(max, left[i-1] + right[i+1]);
         }
-
-        for (int i = 1; i < n-1; i++) {
-            result = Math.max(result, left[i-1] + right[i+1]);
+        for (int i = 1; i <= n; i++) {
+            max = Math.max(max, left[i]);
         }
-
-        System.out.println(result);
+        System.out.println(max);
     }
 }
