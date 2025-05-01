4266c60a3de933082fd3915241cb20947b3a30c2
Engineering-Student-An <chm20060@gmail.com>
2025-05-01 17:20:55 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 288 ms, Memory: 27864 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -1,7 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -11,27 +9,26 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        int[][] arr = new int[n][m];
-
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            String line = st.nextToken();
-
-            for (int j = 0; j < m; j++) {
-                arr[i][j] = line.charAt(j) - '0';
+        int[][] arr = new int[n+1][m+1];
+        for(int i = 1; i <= n; i ++) {
+            String line = br.readLine();
+            for(int j = 1; j <= m; j ++) {
+                arr[i][j] = (int) line.charAt(j-1) - '0';
             }
         }
 
-        int result = -1;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if(i>0 && j>0 && arr[i][j] == 1) {
-                    arr[i][j] = Math.min(arr[i-1][j], Math.min(arr[i][j-1], arr[i-1][j-1])) + 1;
+        int[][] result = new int[n+1][m+1];
+        int max = -1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if(arr[i][j] == 1) {
+                    arr[i][j] = Math.min(Math.min(arr[i-1][j], arr[i][j-1]), arr[i-1][j-1]) + 1;
                 }
-                result = Math.max(result, arr[i][j]);
+
+                max = Math.max(arr[i][j], max);
             }
         }
 
-        System.out.println(result * result);
+        System.out.println(max * max);
     }
 }
