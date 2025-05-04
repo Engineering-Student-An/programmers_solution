d61085679ce755aa9a046893760d34a32cbca3b9
Engineering-Student-An <chm20060@gmail.com>
2025-05-04 20:34:13 +0900

    [Gold III] Title: Dance Dance Revolution, Time: 320 ms, Memory: 48356 KB -BaekjoonHub

--- 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.java
+++ 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.java
@@ -1,62 +1,67 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    static final int INF = 5000000;
-
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String line = br.readLine();
-        String[] nums = line.split(" ");
+        String[] split = line.split(" ");
 
-        int[][] power = {{0, 2, 2, 2, 2},
-                {2, 1, 3, 4, 3},
-                {2, 3, 1, 3, 4},
-                {2, 4, 3, 1, 3},
-                {2, 3, 4, 3, 1}};
-
-        int[][][] arr = new int[nums.length][5][5];
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < 5; j++) {
-                for (int k = 0; k < 5; k++) {
-                    arr[i][j][k] = INF;
-                }
-            }
+        int n = split.length - 1;
+        int[] nums = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            nums[i + 1] = split[i].charAt(0) - '0';
         }
-        arr[0][0][0] = 0;
 
-        for (int i = 0; i < nums.length - 1; i++) {
-
-            int num = nums[i].charAt(0) - '0';
-
-            // 왼발 움직이는 경우
+        int[][][] dp = new int[n + 1][5][5];
+        for (int i = 0; i <= n; i++) {
             for (int j = 0; j < 5; j++) {
-                if(j == num) continue;
                 for (int k = 0; k < 5; k++) {
-                    arr[i+1][num][j] = Math.min(arr[i+1][num][j], arr[i][k][j] + power[k][num]);
-                }
-            }
-
-            // 오른발 움직이는 경우
-            for (int j = 0; j < 5; j++) {
-                if(j == num) continue;
-                for (int k = 0; k < 5; k++) {
-                    arr[i+1][j][num] = Math.min(arr[i+1][j][num], arr[i][j][k] + power[k][num]);
+                    dp[i][j][k] = Integer.MAX_VALUE;
                 }
             }
         }
 
-        int result = INF;
+        if(n > 0) {
+            dp[1][nums[1]][0] = 2;
+            dp[1][0][nums[1]] = 2;
+        }
+
+        for (int i = 1; i < n; i++) {
+            int next = nums[i+1];
+            for (int j = 0; j < 5; j++) {
+                for (int k = 0; k < 5; k++) {
+                    if(dp[i][j][k] != Integer.MAX_VALUE) {
+                        if(next == j || next == k) {
+                            dp[i + 1][j][k] = Math.min(dp[i][j][k] + 1, dp[i + 1][j][k]);
+                        } else {
+                            dp[i + 1][j][next] = Math.min(dp[i][j][k] + count(k, next), dp[i + 1][j][next]);
+                            dp[i + 1][next][k] = Math.min(dp[i][j][k] + count(j, next), dp[i + 1][next][k]);
+                        }
+                    }
+                }
+            }
+        }
+
+        int result = Integer.MAX_VALUE;
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
-                result = Math.min(arr[nums.length - 1][i][j], result);
+                result = Math.min(dp[n][i][j], result);
             }
         }
 
-        System.out.println(result);
+        System.out.println((result == Integer.MAX_VALUE) ? 0 : result);
+    }
+
+    static int count(int now, int next) {
+        if(now == next) return 1;
+        if(now == 0) return 2;
+        if(Math.abs(now - next) == 2) return 4;
+        return 3;
     }
 }
