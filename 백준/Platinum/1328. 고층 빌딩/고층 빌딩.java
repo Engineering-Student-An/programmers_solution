20bfd4bc2ea690c5e41450dc57f307044ebc4ae9
Engineering-Student-An <chm20060@gmail.com>
2025-05-04 15:14:56 +0900

    [Platinum V] Title: 고층 빌딩, Time: 140 ms, Memory: 23368 KB -BaekjoonHub

--- 백준/Platinum/1328. 고층 빌딩/고층 빌딩.java
+++ 백준/Platinum/1328. 고층 빌딩/고층 빌딩.java
@@ -1,28 +1,30 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner scanner = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = scanner.nextInt();
-        int l = scanner.nextInt();
-        int r = scanner.nextInt();
+        int n = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
 
         long[][][] arr = new long[n+1][l+1][r+1];
-
+        long mod = 1000000007;
         arr[1][1][1] = 1;
 
         for (int i = 2; i <= n; i++) {
             for (int j = 1; j <= l; j++) {
                 for (int k = 1; k <= r; k++) {
-                    arr[i][j][k] = (arr[i-1][j-1][k] % 1000000007)
-                            + (arr[i-1][j][k-1] % 1000000007)
-                            + (arr[i-1][j][k] * (i-2) % 1000000007);
+                    arr[i][j][k] = (arr[i-1][j-1][k] + arr[i-1][j][k-1] + (i-2) * arr[i-1][j][k]) % mod;
                 }
             }
         }
 
-        System.out.println(arr[n][l][r] % 1000000007);
+        System.out.println(arr[n][l][r] % mod);
     }
 }
