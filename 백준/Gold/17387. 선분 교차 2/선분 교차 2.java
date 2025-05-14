7b9ac2d59cc331d4f6487ac8212eb3f433b9b042
Engineering-Student-An <chm20060@gmail.com>
2025-05-14 18:14:28 +0900

    [Gold II] Title: 선분 교차 2, Time: 172 ms, Memory: 17696 KB -BaekjoonHub

--- 백준/Gold/17387. 선분 교차 2/선분 교차 2.java
+++ 백준/Gold/17387. 선분 교차 2/선분 교차 2.java
@@ -5,43 +5,38 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        int x1 = scanner.nextInt();
-        int y1 = scanner.nextInt();
-        int x2 = scanner.nextInt();
-        int y2 = scanner.nextInt();
-        int x3 = scanner.nextInt();
-        int y3 = scanner.nextInt();
-        int x4 = scanner.nextInt();
-        int y4 = scanner.nextInt();
+        long x1 = scanner.nextLong();
+        long y1 = scanner.nextLong();
+        long x2 = scanner.nextLong();
+        long y2 = scanner.nextLong();
+        long x3 = scanner.nextLong();
+        long y3 = scanner.nextLong();
+        long x4 = scanner.nextLong();
+        long y4 = scanner.nextLong();
 
-
-        int ccw1 = ccw(x1, y1, x2, y2, x3, y3);
-        int ccw2 = ccw(x1, y1, x2, y2, x4, y4);
-        int ccw3 = ccw(x3, y3, x4, y4, x1, y1);
-        int ccw4 = ccw(x3, y3, x4, y4, x2, y2);
-        int result1 = ccw1 * ccw2;
-        int result2 = ccw3 * ccw4;
-
-        if(result1 == 0 && result2 == 0) {
-            System.out.println(isOverlap(x1, y1, x2, y2, x3, y3, x4, y4) ? 1 : 0);
-        } else if(result1 <= 0 && result2 <= 0) {
+        int ccw_123 = ccw(x1, y1, x2, y2, x3, y3);
+        int ccw_124 = ccw(x1, y1, x2, y2, x4, y4);
+        int ccw_341 = ccw(x3, y3, x4, y4, x1, y1);
+        int ccw_342 = ccw(x3, y3, x4, y4, x2, y2);
+        if(ccw_123 * ccw_124 == 0 && ccw_341 * ccw_342 == 0) {
+            if(Math.min(x1, x2) <= Math.max(x3, x4) && Math.min(x3, x4) <= Math.max(x1, x2) &&
+                    Math.min(y1, y2) <= Math.max(y3, y4) && Math.min(y3, y4) <= Math.max(y1, y2)) {
+                System.out.println(1);
+            } else {
+                System.out.println(0);
+            }
+        } else if(ccw_123 * ccw_124 <= 0 && ccw_341 * ccw_342 <= 0) {
             System.out.println(1);
         } else {
             System.out.println(0);
         }
     }
 
-    static boolean isOverlap(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
+    static int ccw(long x1, long y1, long x2, long y2, long x3, long y3) {
 
-        if(Math.max(x1, x2) < Math.min(x3, x4) || Math.max(x3, x4) < Math.min(x1, x2) ||
-                Math.max(y1, y2) < Math.min(y3, y4) || Math.max(y3, y4) < Math.min(y1, y2)) return false;
-        return true;
-    }
-
-    static int ccw(int x1, int y1, int x2, int y2, int x3, int y3) {
-        long result = ((long) x1 * y2 + (long) x2 * y3 + (long) x3 * y1) - ((long) x2 * y1 + (long) x3 * y2 + (long) x1 * y3);
-        if(result < 0) return -1;
-        else if(result > 0) return 1;
-        else return 0;
+        long ccw = (x1 * y2 + x2 * y3 + x3 * y1) - (x3 * y2 + x2 * y1 + x1 * y3);
+        if(ccw > 0) return 1;
+        else if(ccw == 0) return 0;
+        else return -1;
     }
 }
