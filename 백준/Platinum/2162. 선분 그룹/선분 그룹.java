cbcc2f6682c9988dd45a8c4e3b8286fafc9f6852
Engineering-Student-An <chm20060@gmail.com>
2025-05-15 13:52:20 +0900

    [Platinum V] Title: 선분 그룹, Time: 364 ms, Memory: 19176 KB -BaekjoonHub

--- 백준/Platinum/2162. 선분 그룹/선분 그룹.java
+++ 백준/Platinum/2162. 선분 그룹/선분 그룹.java
@@ -1,3 +1,4 @@
+import javax.sound.sampled.Line;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,97 +6,86 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+    static int n;
     static Line[] arr;
-    static int[] parent;
+    static int[] parents;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
 
         arr = new Line[n];
-        parent = new int[n];
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
+            arr[i] = new Line(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
+        }
 
-            parent[i] = i;
-            arr[i] = new Line(x1, y1, x2, y2);
+        parents = new int[n];
+        for (int i = 0; i < n; i++) {
+            parents[i] = i;
         }
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if(i!=j) {
-                    int ccw1 = ccw(arr[i].x1, arr[i].y1, arr[i].x2, arr[i].y2, arr[j].x1, arr[j].y1);
-                    int ccw2 = ccw(arr[i].x1, arr[i].y1, arr[i].x2, arr[i].y2, arr[j].x2, arr[j].y2);
-                    int ccw3 = ccw(arr[j].x1, arr[j].y1, arr[j].x2, arr[j].y2, arr[i].x1, arr[i].y1);
-                    int ccw4 = ccw(arr[j].x1, arr[j].y1, arr[j].x2, arr[j].y2, arr[i].x2, arr[i].y2);
-                    int result1 = ccw1 * ccw2;
-                    int result2 = ccw3 * ccw4;
-
-                    if (result1 == 0 && result2 == 0) {
-                        if(isOverlap(arr[i].x1, arr[i].y1, arr[i].x2, arr[i].y2, arr[j].x1, arr[j].y1, arr[j].x2, arr[j].y2)) {
-                            union(i, j);
-                        }
-                    } else if (result1 <= 0 && result2 <= 0) {
-                        union(i, j);
-                    }
-                }
+            for (int j = i+1; j < n; j++) {
+                if(isCross(arr[i], arr[j])) union(i, j);
             }
         }
 
-        int[] check = new int[3000];
-        int maxSum = -1;
-        int count = 0;
         for (int i = 0; i < n; i++) {
             find(i);
         }
+
+        int[] count = new int[n];
         for (int i = 0; i < n; i++) {
-            check[parent[i]] ++;
+            count[parents[i]] ++;
         }
 
-        for (int i = 0; i < 3000; i++) {
-            if(check[i] > 0) {
-                count ++;
-                maxSum = Math.max(maxSum, check[i]);
+        int cnt = 0;
+        int max = -1;
+        for (int i = 0; i < n; i++) {
+            if(count[i] != 0) {
+                cnt ++;
+                max = Math.max(max, count[i]);
             }
         }
 
-        System.out.println(count);
-        System.out.println(maxSum);
+        System.out.println(cnt + "\n" + max);
+    }
+    
+    static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        
+        if(a != b) parents[Math.max(a, b)] = Math.min(a, b);
+    }
+    
+    static int find(int a) {
+        if(a == parents[a]) return a;
+        
+        return parents[a] = find(parents[a]);
     }
 
-    static boolean isOverlap(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
+    static boolean isCross(Line one, Line two) {
 
-        if(Math.max(x1, x2) < Math.min(x3, x4) || Math.max(x3, x4) < Math.min(x1, x2) ||
-                Math.max(y1, y2) < Math.min(y3, y4) || Math.max(y3, y4) < Math.min(y1, y2)) return false;
-        return true;
+        int ccw123 = ccw(one.x1, one.y1, one.x2, one.y2, two.x1, two.y1);
+        int ccw124 = ccw(one.x1, one.y1, one.x2, one.y2, two.x2, two.y2);
+        int ccw341 = ccw(two.x1, two.y1, two.x2, two.y2, one.x1, one.y1);
+        int ccw342 = ccw(two.x1, two.y1, two.x2, two.y2, one.x2, one.y2);
+
+        // 두 선분이 견친 경우
+        if(ccw123 * ccw124 == 0 && ccw341 * ccw342 == 0) {
+            return Math.min(one.x1, one.x2) <= Math.max(two.x1, two.x2) && Math.min(two.x1, two.x2) <= Math.max(one.x1, one.x2) &&
+                    Math.min(one.y1, one.y2) <= Math.max(two.y1, two.y2) && Math.min(two.y1, two.y2) <= Math.max(one.y1, one.y2);
+        } else return ccw123 * ccw124 <= 0 && ccw341 * ccw342 <= 0;
     }
 
     static int ccw(int x1, int y1, int x2, int y2, int x3, int y3) {
-        long result = ((long) x1 * y2 + (long) x2 * y3 + (long) x3 * y1) - ((long) x2 * y1 + (long) x3 * y2 + (long) x1 * y3);
+
+        int result = (x1 * y2 + x2 * y3 + x3 * y1) - (x3 * y2 + x2 * y1 + x1 * y3);
         if(result < 0) return -1;
-        else if(result > 0) return 1;
-        else return 0;
-    }
-
-    static void union(int i, int j) {
-        i = find(i);
-        j = find(j);
-
-        if(i!=j) {
-            parent[j] = i;
-        }
-    }
-
-    static int find(int i) {
-        if(parent[i] == i) return i;
-
-        return parent[i] = find(parent[i]);
+        else if(result == 0) return 0;
+        return 1;
     }
 
     static class Line {
