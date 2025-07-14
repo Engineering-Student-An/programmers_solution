9409b995e6e807bffed19c0bed306b4e6ae40d00
Engineering-Student-An <chm20060@gmail.com>
2025-07-14 17:55:31 +0900

    [Gold IV] Title: 불, Time: 836 ms, Memory: 85940 KB -BaekjoonHub

---
+++ 백준/Gold/5427. 불/불.java
@@ -0,0 +1,97 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m;
+    static char[][] arr;
+    static int[] dirRow = {0, 1, 0, -1};
+    static int[] dirCol = {1, 0, -1, 0};
+    static PriorityQueue<Info> queue;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        while(T -- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            m = Integer.parseInt(st.nextToken());
+            n = Integer.parseInt(st.nextToken());
+            arr = new char[n][m];
+
+            queue = new PriorityQueue<>(new Comparator<Info>() {
+                @Override
+                public int compare(Info o1, Info o2) {
+                    if(o1.count == o2.count) {
+                        return o1.type - o2.type;
+                    }
+                    return o1.count - o2.count;
+                }
+            });
+
+            Info start = null;
+            for (int i = 0; i < n; i++) {
+                String line = br.readLine();
+                for (int j = 0; j < m; j++) {
+                    arr[i][j] = line.charAt(j);
+                    if(arr[i][j] == '@') {
+                        start = new Info(i, j, 1, 0);
+                        queue.add(start);
+                        arr[i][j] = '.';
+                    }
+                    else if(arr[i][j] == '*') queue.add(new Info(i, j, 0, 0));
+                }
+            }
+
+            int result = bfs(start);
+            sb.append(result == - 1 ? "IMPOSSIBLE" : result).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+
+    static int bfs(Info start) {
+        boolean[][] visit = new boolean[n][m];
+        visit[start.r][start.c] = true;
+
+        while (!queue.isEmpty()) {
+            Info now = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nr = now.r + dirRow[i];
+                int nc = now.c + dirCol[i];
+
+                if(nr >= 0 && nc >= 0 && nr < n && nc < m) {
+                    if(now.type == 0 && arr[nr][nc] == '.') { // 불 움직임
+                        queue.add(new Info(nr, nc, 0, now.count + 1));
+                        arr[nr][nc] = '*';
+                    } else if(now.type == 1 && !visit[nr][nc] && arr[nr][nc] == '.'){    // 사람 움직임
+                        queue.add(new Info(nr, nc, 1, now.count + 1));
+                        visit[nr][nc] = true;
+                    }
+                } else if(now.type == 1) {
+                    return now.count + 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    static class Info {
+        int r, c, type, count;  // type: 0 -> 불, 1 -> 사람
+
+        public Info(int r, int c, int type, int count) {
+            this.r = r;
+            this.c = c;
+            this.type = type;
+            this.count = count;
+        }
+    }
+}
