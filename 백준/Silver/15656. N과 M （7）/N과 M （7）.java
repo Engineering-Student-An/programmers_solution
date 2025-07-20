08050c16e95f7e4aaf1f15cbe104c204452dc6ae
Engineering-Student-An <chm20060@gmail.com>
2025-07-20 15:55:29 +0900

    [Silver III] Title: N과 M (7), Time: 556 ms, Memory: 123176 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, k;
+    static int[] arr, temp;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n; i++) {
+            temp = new int[k];
+            temp[0] = arr[i];
+            find(i, 1);
+        }
+
+        System.out.print(sb);
+    }
+
+    static void find(int ind, int cnt) {
+        if(cnt == k) {
+            for (int i = 0; i < k; i++) {
+                sb.append(temp[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            temp[cnt] = arr[i];
+            find(i, cnt + 1);
+        }
+    }
+}
