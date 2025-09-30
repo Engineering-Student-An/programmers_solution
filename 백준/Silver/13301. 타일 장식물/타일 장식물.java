0ff2355d182ef8dd370b89d24370129435adcde9
Engineering-Student-An <chm20060@gmail.com>
2025-09-30 21:53:13 +0900

    [Silver V] Title: 타일 장식물, Time: 108 ms, Memory: 14292 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        long[] fibo = new long[81];
+        fibo[0] = 1;
+        fibo[1] = 1;
+
+        for (int i = 2; i <= 80; i++) {
+            fibo[i] = fibo[i-1] + fibo[i-2];
+        }
+
+        long ans = (fibo[n] + fibo[n-1]) * 2;
+        System.out.println(ans);
+    }
+}
