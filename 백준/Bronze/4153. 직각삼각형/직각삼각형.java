e0cf1a57f8db602825dcad0d35ca59b0acc3a36d
Engineering-Student-An <chm20060@gmail.com>
2025-10-02 14:29:58 +0900

    [Bronze III] Title: 직각삼각형, Time: 100 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+        while(true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int[] arr = new int[3];
+            for (int i = 0; i < 3; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+
+            if(arr[0] == 0 && arr[1] == 0 && arr[2] == 0) break;
+
+            Arrays.sort(arr);
+
+            if((int) Math.pow(arr[0], 2) + Math.pow(arr[1], 2) == (int) Math.pow(arr[2], 2)) sb.append("right\n");
+            else sb.append("wrong\n");
+        }
+
+        System.out.print(sb);
+
+    }
+}
