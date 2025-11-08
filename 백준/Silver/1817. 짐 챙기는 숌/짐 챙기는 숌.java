470706728e6bce71740ff213afd2968aebbd8b34
Engineering-Student-An <chm20060@gmail.com>
2025-11-08 12:38:06 +0900

    [Silver V] Title: 짐 챙기는 숌, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Silver/1817. 짐 챙기는 숌/짐 챙기는 숌.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int count = 0;
+        if(n > 0) {
+            st = new StringTokenizer(br.readLine());
+            count ++;
+            int w = Integer.parseInt(st.nextToken());
+            for (int i = 1; i < n; i++) {
+                int now = Integer.parseInt(st.nextToken());
+
+                if (w + now > k) {
+                    count++;
+                    w = now;
+                } else {
+                    w += now;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
