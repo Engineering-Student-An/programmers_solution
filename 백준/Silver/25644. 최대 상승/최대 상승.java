88651df11273e1f5c434185c7c38f7a32953189d
Engineering-Student-An <chm20060@gmail.com>
2025-11-29 10:32:25 +0900

    [Silver V] Title: 최대 상승, Time: 328 ms, Memory: 34224 KB -BaekjoonHub

---
+++ 백준/Silver/25644. 최대 상승/최대 상승.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int result = 0;
+        int min = Integer.MAX_VALUE;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+
+            min = Math.min(min, num);
+            result = Math.max(result, (num - min));
+        }
+
+        System.out.println(result);
+    }
+}
